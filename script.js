6586c281efbe09b1fa113c25da57c99471f752cf
kokocat1313 <66399733+kokocat1313@users.noreply.github.com>
2020-10-25 14:53:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // DEFINE YOUR FUNCTION:
 function rant(message){
-
     console.log("i hate beets".toUpperCase());
     console.log("i hate beets".toUpperCase());
-    console.log("i hate beets".toUpperCase());
-                
+    console.log("i hate beets".toUpperCase());           
   }
   rant("message");
